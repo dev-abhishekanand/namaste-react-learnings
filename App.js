fe43586a0c83aec8ff4255e318ffc310905a9636
dev-abhishekanand <abhishek.anand.7601@gmail.com>
2025-03-27 22:35:58 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,16 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    React.createElement("h1", { key: "h1-child1" }, "I am an H1 tag"),
-    React.createElement("h2", { key: "h2-child1" }, "I am an H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "h1-child2" }, "I am an H1 tag"),
-    React.createElement("h2", { key: "h2-child2" }, "I am an H2 tag"),
-  ]),
-]);
+// episode-2
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1", key: "child1" }, [
+//     React.createElement("h1", { key: "h1-child1" }, "I am an H1 tag"),
+//     React.createElement("h2", { key: "h2-child1" }, "I am an H2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2", key: "child2" }, [
+//     React.createElement("h1", { key: "h1-child2" }, "I am an H1 tag"),
+//     React.createElement("h2", { key: "h2-child2" }, "I am an H2 tag"),
+//   ]),
+// ]);
 
-var root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// var root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+// React Element
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namaste React..."
+// );
+
+// episode-3
+const heading = (
+  <h1 id="heading" className="head">
+    Namaste React Learnings with JSX
+  </h1>
+);
+
+//  React Components
+// react component through function
+const Title = function () {
+  return <h1> this is title</h1>;
+};
+
+// react component through arrow function
+const HeadingComponent = () => {
+  return (
+    <>
+      <Title />
+      <h1>Namaste React Functional component</h1>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
