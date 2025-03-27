7797c1449ccc37190483069ac007d45739d567bb
dev-abhishekanand <abhishek.anand.7601@gmail.com>
2025-03-27 16:33:57 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-var parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am an H1 tag"),
-    React.createElement("h2", {}, "I am an H2 tag"),
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1-child1" }, "I am an H1 tag"),
+    React.createElement("h2", { key: "h2-child1" }, "I am an H2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an H1 tag"),
-    React.createElement("h2", {}, "I am an H2 tag"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-child2" }, "I am an H1 tag"),
+    React.createElement("h2", { key: "h2-child2" }, "I am an H2 tag"),
   ]),
 ]);
+
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
